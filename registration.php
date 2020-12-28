d74a9cefb4aa3626c32fee75d26e27e71c7c2957
SolwinInfotech <stdabhoya@yahoo.com>
2020-12-28 10:57:07 +0530

    initial relase

---
+++ registration.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Solwin Infotech
+ * Solwin Print Order PDF Extension
+ *
+ * @category   Solwin
+ * @package    Solwin_PrintOrderPdf
+ * @copyright  Copyright © 2006-2020 Solwin (https://www.solwininfotech.com)
+ * @license    https://www.solwininfotech.com/magento-extension-license/
+ */
+?>
+<?php
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Solwin_PrintOrderPdf',
+    __DIR__
+);
